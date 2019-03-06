68f827070214a20b8ad11efe2b4a9ff7e2004553
hieunguyen061197 <hieunguyen0611977@gmail.com>
2019-03-06 09:32:01 +0700

    sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     get "/help", to: "static_pages#help"
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
+    get "/sp1", to: "static_pages#sp1"
+    get "/sp2", to: "static_pages#sp2"
+    get "/sp3", to: "static_pages#sp3"
+    get "/sp4", to: "static_pages#sp4"
     resources :users
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_023541) do
+ActiveRecord::Schema.define(version: 2019_03_06_012546) do
 
   create_table "bills", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.integer "user_id"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2019_02_27_023541) do
     t.integer "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
